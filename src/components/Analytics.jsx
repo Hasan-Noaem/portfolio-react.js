d436e9909cf1b50b281e5115db603594e3a9e740
Hasan-Noaem <122913504+Hasan-Noaem@users.noreply.github.com>
2023-02-13 11:27:47 -0800

    p

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Laptop from "../assets/laptop.jpg";
+
+const Analytics = () => {
+    return (
+    <div className="w-full bg-white py-16 px-4">
+        <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+            <img className="w-[500px] mx-auto my-4" src={Laptop} alt="/" />
+            <div className="flex flex-col justify-center">
+                <div className="max-sm:text-center">
+                <p className="text-[#00df9a] font-bold">DATA ANALYTICS DASHBOARD</p>
+                <h1 className="md:text-4xl sm:text-3xl font-bold py-2">Manage Data Analytics Centrally</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt corporis dolor dolorum cupiditate quos unde esse odit, ad iure magni et rem explicabo provident ipsam impedit nemo nulla! Tempore, deleniti.</p>
+                <button className="bg-[#000300] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 text-[#00df9a]">Get Started</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Analytics;
